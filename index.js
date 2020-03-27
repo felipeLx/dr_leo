f00c0c33d65e584eaf51594cfc5344be3a915eb4
felipeLx <felipealvesfinanceiro@gmail.com>
2020-03-27 19:06:59 +0000

    pics size; heart count

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let clicked = document.getElementById("clickme");
 let count = 0;
 
-clicked.onclick = function() {
-    count += 1;
-    clicked.innerHTML = count;
+function onClick() {
+    console.log('clicked');
+    if(typeof(Storage) !== "undefined") {
+        if(localStorage.clickcount) {
+            count += 1;
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        return document.getElementById("clickme").innerHTML = count; 
+    }
+    
+    
 }
+
